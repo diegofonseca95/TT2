cd6e3d9769be89aab56a2dfe3c9ff73460ac8c52
diegofonseca95 <diegofonseca95@users.noreply.github.com>
2018-09-13 17:09:18 -0500

    Inicio de sesion

--- app/Http/Controllers/UsuariosControlador.php
+++ app/Http/Controllers/UsuariosControlador.php
@@ -21,15 +21,22 @@ class UsuariosControlador extends Controller
         return view('admin_users');
     }
     public function agregarUsuarioBD(){
+        if(!Auth::check()){
+            return response()->json([
+                'status'=> 'ERROR',
+                'result'=> 'Inicia sesion para continuar'
+                ]);
+        }
     	$usuario = new User;
         $query = $usuario->where([['correo', '=', request('correo')]])->get();
 
-        if($query->isNotEmpty()){
+        if($query->isNotEmpty() ){
             return response()->json([
                 'status'=> 'ERROR',
                 'result'=> 'Ya existe una cuenta con ese correo'
                 ]);
         }
+
 
     	$usuario->nombre = request('nombre');
     	$usuario->apellidoPaterno = request('apellidoPaterno');
@@ -48,6 +55,13 @@ class UsuariosControlador extends Controller
     }
 
     public function obtenerUsuario(){
+        if(!Auth::check()){
+            return response()->json([
+                'status'=> 'ERROR',
+                'result'=> 'Inicia sesion para continuar'
+                ]);
+        }
+
         $usuario = new User;
         $query = $usuario->where([['idUsuario','=', request('idUsuario')]]);
 
@@ -55,6 +69,13 @@ class UsuariosControlador extends Controller
     }
 
     public function obtenerUsuarios(){
+        if(!Auth::check()){
+            return response()->json([
+                'status'=> 'ERROR',
+                'result'=> 'Inicia sesion para continuar'
+                ]);
+        }
+        
         $usuario = new User;
         
         return $usuario->where('estado', '!=', 3)->get();
