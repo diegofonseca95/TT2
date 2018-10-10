d7cf8d16f928e7745c190b5a675ddf5367087b25
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-10-09 23:49:59 -0500

    Fixes?

--- public/js/components/admin-watch-group/new-leader-list.js
+++ public/js/components/admin-watch-group/new-leader-list.js
@@ -14,21 +14,19 @@ Vue.component('new-leader-list', {
         <span class="card-title first-text">
           <b>Seleccionar líder</b>
         </span>
-        <form action="#">
-          <ul class="collection">
-            <new-leader-list-item
-              v-for="member in groupMembers"
-              @new-leader-chosen="handleNewLeader($event)"
-              :is-checked="leaderId === member.idUsuario"
-              :key="member.idUsuario"
-              :member="member">
-            </new-leader-list-item>
-            <li class="collection-item"
-              v-if="groupMembers.length === 0">
-              No hay miembros en el grupo.
-            </li>
-          </ul>
-        </form>
+        <ul class="collection">
+          <new-leader-list-item
+            v-for="member in groupMembers"
+            @new-leader-chosen="handleNewLeader($event)"
+            :is-checked="leaderId === member.idUsuario"
+            :key="member.idUsuario"
+            :member="member">
+          </new-leader-list-item>
+          <li class="collection-item"
+            v-if="groupMembers.length === 0">
+            No hay miembros en el grupo.
+          </li>
+        </ul>
       </div>
     </div>
   `
