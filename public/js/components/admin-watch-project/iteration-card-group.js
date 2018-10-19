d4ae361326934c1dadce836255d07d7100c5b473
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-10-18 19:00:02 -0500

    Fetching the sprint list.

--- public/js/components/admin-watch-project/iteration-card-group.js
+++ public/js/components/admin-watch-project/iteration-card-group.js
@@ -5,7 +5,34 @@ Vue.component('iteration-card-group', {
     };
   },
   beforeCreate : function(){
-    // TODO : Fetch iteration list.
+    // Get the project id from the hidden input.
+    var projectIdInput = document.querySelector('input[name="project-id"]');
+
+    var authToken = document.querySelector('input[name="_token"]');
+
+    // Request data for the 'fetch' function.
+    var requestData = {
+      headers: { 'Content-Type' : 'application/json' },
+      method : 'POST'
+    };
+
+    // The body of our request.
+    var requestBody = { 
+      idProyecto : projectIdInput.value,
+      _token : authToken.value
+    };
+
+    requestData.body = JSON.stringify(requestBody);
+
+    // Fetch the users list.
+    fetch('/obtenerSprints', requestData)
+    .then(response => response.json())
+    .then(function(response){
+      if(response.status === 'OK'){
+        this.iterations = response.result;
+      }
+      // TODO : Handle non 'OK' status.
+    }.bind(this));
   },
   methods : {
     handleIterationCreated : function(iteration){
