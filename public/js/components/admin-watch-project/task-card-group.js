5fd0a6a3ea35ce2fddb6a901d5f53b0c3eda8008
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-10-22 19:07:07 -0500

    Remove task.

--- public/js/components/admin-watch-project/task-card-group.js
+++ public/js/components/admin-watch-project/task-card-group.js
@@ -50,8 +50,35 @@ Vue.component('task-card-group', {
         this.assignedTasksIds.push(task.idTarea);
       }
     },
-    handleRemoveTask : function(task){
-      // TODO : Remove the task.
+    handleRemoveTask : function(removeTask){
+      var authToken = document.querySelector('input[name="_token"]');
+
+      // Request data for the 'fetch' function.
+      var requestData = {
+        headers: { 'Content-Type' : 'application/json' },
+        method : 'POST'
+      };
+
+      // The body of our request.
+      var requestBody = { 
+        idTarea : removeTask.idTarea,
+        _token : authToken.value
+      };
+
+      requestData.body = JSON.stringify(requestBody);
+
+      // Send new task to the server.
+      fetch('/eliminarTarea', requestData)
+      .then(response => response.json())
+      .then(function(response){
+        if(response.status === 'OK'){
+          this.tasks = this.tasks.filter(
+            task => task.idTarea !=== removeTask.idTarea
+          );
+          SuccessToast(response.result);
+        }
+        // TODO : Handle non 'OK' status.
+      }.bind(this));
     },
     handleTaskCreated : function(task){
       this.tasks.push(task);
