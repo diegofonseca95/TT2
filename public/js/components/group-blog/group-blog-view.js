f84c86fb7c8786c1fee858396e2e5d5110523d27
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-05 20:09:42 -0600

    Fetching posts list.

--- public/js/components/group-blog/group-blog-view.js
+++ public/js/components/group-blog/group-blog-view.js
@@ -8,8 +8,45 @@ Vue.component('group-blog-view', {
       posts : []          // The group posts.
     };
   },
+  beforeCreate : function(){
+    // Get the group id from the hidden input.
+    var groupIdInput = document.querySelector('input[name="group-id"]');
+
+    var authToken = document.querySelector('input[name="_token"]');
+
+    // Request data for the 'fetch' function.
+    var requestData = {
+      headers: { 'Content-Type' : 'application/json' },
+      method : 'POST'
+    };
+
+    // The body of our request.
+    var requestBody = { 
+      idGrupo : groupIdInput.value,
+      _token : authToken.value
+    };
+
+    requestData.body = JSON.stringify(requestBody);
+
+    // Send the new post to the server.
+    fetch('/obtenerPublicaciones', requestData)
+    .then(response => response.json())
+    .then(function(response){
+      if(response.status === 'OK'){
+        var postsList = {};
+        for(var i in response.result){
+          var post = response.result[i];
+          post.author = response.usuarios[post.idUsuario];
+          postsList.push(post);
+        }
+        this.posts = postsList;
+      }else{
+        WarningToast(response.result);
+      }
+    }.bind(this));
+  },
   computed : {
-    postsList : function(){
+    orderedList : function(){
       return posts.reverse();
     }
   },
@@ -68,7 +105,7 @@ Vue.component('group-blog-view', {
         @post-submitted="handlePostSubmitted">
       </new-post-card>
       <group-post
-        v-for="post in postsList"
+        v-for="post in orderedList"
         @post-accepted="handlePostAccepted"
         @post-rejected="handlePostRejected"
         @post-selected="handlePostSelected"
