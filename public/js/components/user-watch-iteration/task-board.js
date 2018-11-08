844df6c2dbd9380a27f3d895e6c63b2598365293
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-07 21:25:58 -0600

    Logging.

--- public/js/components/user-watch-iteration/task-board.js
+++ public/js/components/user-watch-iteration/task-board.js
@@ -31,7 +31,7 @@ Vue.component('task-board', {
     fetch('/obtenerTareasSprint', requestData)
     .then(response => response.json())
     .then(function(response){
-      console.log(response.result);
+      console.log(response);
       if(response.status === 'OK'){
         var taskInfo = [];
         for(var i in response.result){
