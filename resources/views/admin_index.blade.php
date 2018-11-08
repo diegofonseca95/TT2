ae7f6b613893a0e22039ebc0713c061485997dd7
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-07 18:56:42 -0600

    Updated paths.

--- resources/views/admin_index.blade.php
+++ resources/views/admin_index.blade.php
@@ -5,8 +5,8 @@
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css"
     media="screen,projection">
-  <link rel="stylesheet" type="text/css" href="../../public/css/system_colors.css">
-  <link rel="stylesheet" type="text/css" href="../../public/css/helpers.css">
+  <link rel="stylesheet" type="text/css" href="./css/system_colors.css">
+  <link rel="stylesheet" type="text/css" href="./css/helpers.css">
   <title>Bienvenido</title>
 
   <meta charset="utf-8">
@@ -44,7 +44,7 @@
       <div class="col s12 m4">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="../../public/img/users.jpg">
+            <img class="activator" src="./img/users.jpg">
           </div>
           <div class="card-content">
             <span class="card-title activator grey-text text-darken-4">Usuarios<i class="material-icons right">more_vert</i></span>
@@ -65,7 +65,7 @@
       <div class="col s12 m4">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="../../public/img/groups.jpg">
+            <img class="activator" src="./img/groups.jpg">
           </div>
           <div class="card-content">
             <span class="card-title activator grey-text text-darken-4">Grupos<i class="material-icons right">more_vert</i></span>
@@ -85,7 +85,7 @@
       <div class="col s12 m4">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="../../public/img/projects.jpg">
+            <img class="activator" src="./img/projects.jpg">
           </div>
           <div class="card-content">
             <span class="card-title activator grey-text text-darken-4">Proyectos<i class="material-icons right">more_vert</i></span>
@@ -107,7 +107,7 @@
       <div class="col s12 m4">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="../../public/img/blog.JPG">
+            <img class="activator" src="./img/blog.JPG">
           </div>
           <div class="card-content">
             <span class="card-title activator grey-text text-darken-4">Blogs<i class="material-icons right">more_vert</i></span>
@@ -127,7 +127,7 @@
       <div class="col s12 m4">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="../../public/img/statistics.jpg">
+            <img class="activator" src="./img/statistics.jpg">
           </div>
           <div class="card-content">
             <span class="card-title activator grey-text text-darken-4">Estadísticas<i class="material-icons right">more_vert</i></span>
@@ -147,7 +147,7 @@
       <div class="col s12 m4">
         <div class="card">
           <div class="card-image waves-effect waves-block waves-light">
-            <img class="activator" src="../../public/img/files.jpg">
+            <img class="activator" src="./img/files.jpg">
           </div>
           <div class="card-content">
             <span class="card-title activator grey-text text-darken-4">Archivos<i class="material-icons right">more_vert</i></span>
@@ -173,7 +173,7 @@
   <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/jquery.validate.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.17.0/dist/additional-methods.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
-  <script src="../../public/js/toasts.js"></script>
+  <script src="./js/toasts.js"></script>
   <script>
     //M.AutoInit();
   </script>
