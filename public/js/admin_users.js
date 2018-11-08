011c0252cbfa884aba19b8837ae1fac5e2017bb0
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-07 19:42:14 -0600

    User info in users list.

--- public/js/admin_users.js
+++ public/js/admin_users.js
@@ -1,37 +1,37 @@
 var authToken = '';
 
 $(document).ready(function(){
-    authToken = $("[name = '_token']").val();
-    var app = new Vue({
-        el : "#admin-users-box",
+  authToken = $("[name = '_token']").val();
+  var app = new Vue({
+    el : "#admin-users-box",
+    data : {
+      displayedUser : {},
+      users : []
+    },
+    methods : {
+      handleMemberSelected : function(selectedUser){
+        console.log(selectedUser);
+        this.displayedUser = selectedUser;
+        M.Modal.getInstance(
+          document.querySelector('#user-info-modal')
+          ).open();
+      }
+    },
+    mounted : function(){
+      $.ajax({
+        url : '/obtenerUsuarios',
+        method : 'POST',
         data : {
-            displayedUser : {},
-            users : []
-        },
-        methods : {
-          handleMemberSelected : function(selectedUser){
-            console.log(selectedUser);
-            this.displayedUser = selectedUser;
-            M.Modal.getInstance(
-              document.querySelector('#user-info-modal')
-            ).open();
-          }
-        },
-        mounted : function(){
-            $.ajax({
-                url : '/obtenerUsuarios',
-                method : 'POST',
-                data : {
-                    _token : authToken
-                }
-            }).done(function(response){
-                console.log(response);
-                this.users = response.map(user => {
-                    user.valid = (user.estado == 1);
-                    return user;
-                });
-            }.bind(this));
+          _token : authToken
         }
-    });
-    M.AutoInit();
+      }).done(function(response){
+        console.log(response);
+        this.users = response.map(user => {
+          user.valid = (user.estado == 1);
+          return user;
+        });
+      }.bind(this));
+    }
+  });
+  M.AutoInit();
 });
