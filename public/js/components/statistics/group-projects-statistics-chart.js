8b2f40433b3a17f43a08dd7e76f047cc156143e9
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-15 20:15:45 -0600

    Drawing inside response.

--- public/js/components/statistics/group-projects-statistics-chart.js
+++ public/js/components/statistics/group-projects-statistics-chart.js
@@ -1,5 +1,5 @@
 Vue.component('group-projects-statistics-chart', {
-  beforeCreate : function(){
+  mounted : function(){
     var authToken = document.querySelector('input[name="_token"]');
 
     // Request data for the 'fetch' function.
@@ -32,21 +32,23 @@ Vue.component('group-projects-statistics-chart', {
         }
         // Draw the chart.
         google.charts.load('current', { 'packages' : ['bar'] });
-        var data = [['Grupos', 'Proyectos']];
-        points.map(point => {
-          data.push([point.x, point.y]);
+        google.charts.setOnLoadCallback(function(){
+          var data = [['Grupos', 'Proyectos']];
+          points.map(point => {
+            data.push([point.x, point.y]);
+          });
+          var table = new google.visualization.arrayToDataTable(data);
+          var options = {
+            title : 'Proyectos por Grupo',
+            bars : 'horizontal'
+          };
+          var chart = new google.charts.Bar(
+            document.querySelector(
+              '#group-projects-statistics-chart'
+            )
+          );
+          chart.draw(table, options);
         });
-        var table = new google.visualization.arrayToDataTable(data);
-        var options = {
-          title : 'Proyectos por Grupo',
-          bars : 'horizontal'
-        };
-        var chart = new google.charts.Bar(
-          document.querySelector(
-            '#group-projects-statistics-chart'
-          )
-        );
-        chart.draw(table, options);
       }
       // TODO : Handle non 'OK' status.
     }.bind(this));
