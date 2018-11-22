ca66faa820ed76633ebd01d06d89497b808af8c5
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-21 20:46:34 -0600

    Color.

--- public/js/components/admin-watch-project/recent-activity-log.js
+++ public/js/components/admin-watch-project/recent-activity-log.js
@@ -54,6 +54,10 @@ Vue.component('recent-activity-log', {
     this.channel.bind('App\\Events\\Logs', function(data) {
       this.activities.unshift(data.actividad);
       this.newCount++;
+      var List = document.querySelector(
+        '#recent-activity-log-list'
+      );
+      List.scrollTop = 0;
     }.bind(this));
   },
   mounted : function(){
@@ -74,14 +78,15 @@ Vue.component('recent-activity-log', {
       <li>
         <div class="collapsible-header">
           <i class="material-icons">autorenew</i>Actividad Reciente
-          <span class="new badge red" data-badge-caption="nuevas"
+          <span class="new badge blue" data-badge-caption="nuevas"
             v-if="newCount > 0">
             {{ newCount }}
           </span>
         </div>
         <div class="collapsible-body zero-padding">
           <!-- Logs List Begins-->
-          <ul class="collection scrollable-collection">
+          <ul class="collection scrollable-collection"
+            id="recent-activity-log-list">
             <!-- No Logs to show item begins-->
             <li class="collection-item" 
               v-if="activities.length === 0">
