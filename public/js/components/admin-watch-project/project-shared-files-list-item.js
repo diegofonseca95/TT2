7eef14d26a49412e35b74e81e953718cd1a0fcdc
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-26 17:59:26 -0600

    File download.

--- public/js/components/admin-watch-project/project-shared-files-list-item.js
+++ public/js/components/admin-watch-project/project-shared-files-list-item.js
@@ -2,6 +2,16 @@ Vue.component('project-shared-files-list-item', {
   props : [
     'file'  // The file information.
   ],
+  computed : {
+    downloadLink : function(){
+      // Get the project id from the hidden input.
+      var projectInput = document.querySelector(
+        'input[name="project-id"]'
+      );
+      return '/descargarArchivo/' + projectInput.value +
+        '/file/' + file.fileName;
+    }
+  },
   template : `
     <li class="collection-item avatar">
       <i class="material-icons circle third-background">attach_file</i>
@@ -10,7 +20,7 @@ Vue.component('project-shared-files-list-item', {
           {{ file.fileName }}
         </span>
         <span class="title truncate col s12">
-          <a href="#">
+          <a :href="downloadLink" target="_blank">
             Descargar Archivo
           </a>
         </span>
