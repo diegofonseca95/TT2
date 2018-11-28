6c72c8e3ad8f537f9435447aa93c518733697c8d
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-27 19:01:11 -0600

    Stuff.

--- public/js/components/group-blog/new-post-card.js
+++ public/js/components/group-blog/new-post-card.js
@@ -47,7 +47,15 @@ Vue.component('new-post-card', {
     $('#new-post-card-form').validate(formSettings);
     // Initialize editor.
     tinymce.init({
-      selector : '#new-post-card-content-input'
+      selector : '#new-post-card-content-input',
+      theme : 'modern',
+      plugins : 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
+      toolbar1 : 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+      image_advtab : true,
+      content_css : [
+        '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+        '//www.tinymce.com/css/codepen.min.css'
+      ]
     });
     tinymce.get('new-post-card-content-input').setContent('');
   },
