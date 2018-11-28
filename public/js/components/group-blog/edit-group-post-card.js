1ec5c6ce4f5ce3475953d6e0323d75503b891a2d
Ant4g0n1st <dragon124victor421force@gmail.com>
2018-11-27 20:58:07 -0600

    .

--- public/js/components/group-blog/edit-group-post-card.js
+++ public/js/components/group-blog/edit-group-post-card.js
@@ -30,7 +30,6 @@ const EditGroupPostCardTinyMCESettings = {
   toolbar1 : 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
   image_advtab : true,
   theme : 'modern',
-  inline : true,
   content_css : [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
     '//www.tinymce.com/css/codepen.min.css'
